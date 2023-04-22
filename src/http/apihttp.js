85eff5c2abe63a6e14853501b449975ea66ffa0e
Yldemaro Jimenez <ylde198989@gmail.com>
2023-04-22 16:04:34 -0400

    estilos

--- src/http/apihttp.js
+++ src/http/apihttp.js
@@ -1,5 +1,6 @@
-// const baseUrl= "http://3.15.149.136:3555/api";
-const baseUrl= "http://localhost:5555/api";
+const baseUrl= "http://3.15.149.136:3555/api";
+
+// const baseUrl= "http://localhost:5555/api";
 
 
 
